e058a53c95eaef1fe2a82c2838e360bfe88af7bc
akulaswathi1 <143166122+akulaswathi1@users.noreply.github.com>
2023-08-25 12:40:43 +0530

    Add files via upload

---
+++ palindrome.js
@@ -0,0 +1,12 @@
+function apple() {
+
+    let a = "madam";
+    var result = a.split('').reverse().join('')
+    if(result!=a){
+        console.log("false")
+    }else{
+        console.log("True")
+    }
+
+}
+apple()
